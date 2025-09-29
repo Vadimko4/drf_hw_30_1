91111eb01fc8cbb4ea529800c6b607c4bb592cdb
vadimko4 <vad.informat@gmail.com>
2025-09-30 00:15:50 +0400

    add users app and user model

--- config/settings.py
+++ config/settings.py
@@ -26,6 +26,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -101,3 +102,5 @@ MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'users.User'
---
+++ users/models.py
@@ -0,0 +1,20 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+class User(AbstractUser):
+    username = None
+
+    email = models.EmailField(unique=True, verbose_name="Почта", help_text="Укажите почту")
+    phone = models.CharField(max_length=35, verbose_name="Телефон", blank=True, null=True,
+                             help_text="Введите номер телефона")
+    town = models.CharField(max_length=50, verbose_name="Город", blank=True, null=True,
+                               help_text="Введите город")
+    avatar = models.ImageField(upload_to="users/avatars/", verbose_name="Аватар", blank=True, null=True,
+                               help_text="Загрузите свой аватар")
+
+    USERNAME_FIELD = "email" # меняем юзернейм на почту
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
