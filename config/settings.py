7ff221a3625d0b9d0a732dc6d49f9bc1bb37bd46
vadimko4 <vad.informat@gmail.com>
2025-09-30 00:38:26 +0400

    add materials app and course, lesson models

--- config/settings.py
+++ config/settings.py
@@ -27,6 +27,7 @@ INSTALLED_APPS = [
 
     'rest_framework',
     'users',
+    'materials',
 ]
 
 MIDDLEWARE = [
---
+++ materials/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+
+
+class Course(models.Model):
+    title = models.CharField(max_length=100, verbose_name='Название', help_text='Введите название курса')
+    preview = models.ImageField(upload_to="materials/courses_preview/", verbose_name="Превью", blank=True, null=True,
+                               help_text="Загрузите превью курса")
+    description = models.TextField(blank=True, null=True, verbose_name='Описание курса',
+                                   help_text='Введите описание курса')
+
+    class Meta:
+        verbose_name = "Курс"
+        verbose_name_plural = "Курсы"
+
+    def __str__(self):
+        return self.title
+
+
+class Lesson(models.Model):
+    title = models.CharField(max_length=100, verbose_name='Название', help_text='Введите название урока')
+    description = models.TextField(blank=True, null=True, verbose_name='Описание урока',
+                                   help_text='Введите описание урока')
+    preview = models.ImageField(upload_to="materials/lessons_preview/", verbose_name="Превью", blank=True, null=True,
+                                help_text="Загрузите превью урока")
+    video_link = models.URLField(max_length=200, blank=True, null=True, verbose_name='Ссылка на видео урока',
+                                   help_text='Введите ссылку на видео урока')
+    course = models.ForeignKey('Course', on_delete=models.SET_NULL, verbose_name='Курс',
+                        help_text='Введите курс', blank=True, null=True)
+
+    class Meta:
+        verbose_name = "Урок"
+        verbose_name_plural = "Уроки"
+
+    def __str__(self):
+        return self.title
