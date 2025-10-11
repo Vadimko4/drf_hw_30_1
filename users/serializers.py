26908be991196dfaced99a5d7b26a14f6ece749b
vadimko4 <vad.informat@gmail.com>
2025-10-11 21:19:35 +0400

    add working with users

--- users/serializers.py
+++ users/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework.serializers import ModelSerializer, SerializerMethodField
-from users.models import Payment
+from users.models import Payment, User
 
 
 class PaymentSerializer(ModelSerializer):
@@ -18,4 +18,10 @@ class PaymentDetailSerializer(ModelSerializer):
 
     class Meta:
         model = Payment
+        fields = "__all__"
+
+
+class UserSerializer(ModelSerializer):
+    class Meta:
+        model = User
         fields = "__all__"
--- users/views.py
+++ users/views.py
@@ -1,9 +1,11 @@
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters
+from rest_framework.generics import CreateAPIView
+from rest_framework.permissions import AllowAny
 from rest_framework.viewsets import ModelViewSet
 
-from users.models import Payment
-from users.serializers import PaymentDetailSerializer, PaymentSerializer
+from users.models import Payment, User
+from users.serializers import PaymentDetailSerializer, PaymentSerializer, UserSerializer
 
 
 class PaymentViewSet(ModelViewSet):
@@ -17,3 +19,21 @@ class PaymentViewSet(ModelViewSet):
         if self.action == "retrieve":
             return PaymentDetailSerializer
         return PaymentSerializer
+
+
+# from rest_framework.generics import CreateAPIView
+# from rest_framework.permissions import AllowAny
+#
+# from users.models import User
+# from users.serializers import UserSerializer
+
+
+class UserCreateAPIView(CreateAPIView):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+    permission_classes = (AllowAny,)
+
+    def perform_create(self, serializer):
+        user = serializer.save(is_active=True)
+        user.set_password(user.password) #  устанавливаем пароль пользователю в захешированном виде
+        user.save()
