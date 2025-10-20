5c562274615e6243da64d5a8f8df3d73705d5c3e
Vadimko4 <vad.informat@google.com>
2025-10-21 00:28:50 +0400

    task 3

--- materials/views.py
+++ materials/views.py
@@ -2,12 +2,14 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.generics import CreateAPIView, ListAPIView, RetrieveAPIView, UpdateAPIView, DestroyAPIView
 from materials.models import Lesson, Course
+from materials.paginations import CustomPagination
 from materials.serializers import LessonSerializer, CourseSerializer, CourseDetailSerializer
 from users.permissions import IsModer, IsOwner
 
 
 class CourseViewSet(ModelViewSet):
-    # queryset = Course.objects.all()
+    pagination_class = CustomPagination
+
     def get_queryset(self):
         user = self.request.user
 
@@ -54,6 +56,8 @@ class LessonListAPIView(ListAPIView):
     # queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
     permission_classes = (IsAuthenticated, IsModer | IsOwner)
+    pagination_class = CustomPagination
+
 
     def get_queryset(self):
         # Если пользователь модератор - показываем все уроки
