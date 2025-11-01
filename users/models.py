2d8be194807344b18a10003b874974826c4bb329
Vadimko4 <vad.informat@google.com>
2025-11-01 10:34:19 +0400

    update Payment model

--- users/models.py
+++ users/models.py
@@ -34,15 +34,50 @@ class Payment(models.Model):
         ('transaction', 'Перевод'),
     ]
 
-    user = models.ForeignKey(User, verbose_name='Плательщик', help_text='Укажите плательщика',
-                             on_delete=models.CASCADE)
-    course = models.ForeignKey(Course, on_delete=models.CASCADE, verbose_name='Курс',
-                               help_text='Укажите курс')
-    lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE, verbose_name='Оплаченный урок',
-                               help_text='Укажите оплаченный урок')
-    date = models.DateField(verbose_name='Дата оплаты', help_text='Введите дату оплаты')
-    amount = models.PositiveIntegerField(verbose_name='Сумма оплаты', help_text='Введите сумму оплаты')
-    type = models.CharField(max_length=12, choices=PAYMENT_TYPE)
+    user = models.ForeignKey(
+        User,
+        verbose_name='Плательщик',
+        help_text='Укажите плательщика',
+        on_delete=models.CASCADE
+    )
+    course = models.ForeignKey(
+        Course,
+        on_delete=models.CASCADE,
+        verbose_name='Курс',
+        help_text='Укажите курс'
+    )
+    lesson = models.ForeignKey(
+        Lesson,
+        on_delete=models.CASCADE,
+        verbose_name='Оплаченный урок',
+        help_text='Укажите оплаченный урок'
+    )
+    date = models.DateField(
+        verbose_name='Дата оплаты',
+        help_text='Введите дату оплаты'
+    )
+    amount = models.PositiveIntegerField(
+        verbose_name='Сумма оплаты',
+        help_text='Введите сумму оплаты'
+    )
+    type = models.CharField(
+        max_length=12,
+        choices=PAYMENT_TYPE
+    )
+    session_id = models.CharField(
+        max_length=255,
+        verbose_name="Id сессии",
+        help_text="Введите id сессии",
+        blank=True,
+        null=True,
+    )
+    link = models.URLField(
+        max_length=400,
+        verbose_name="Ссылка на оплату",
+        help_text="Укажите ссылку на оплату",
+        blank=True,
+        null=True,
+    )
 
     def __str__(self):
         return f'{self.user}: {self.amount}, {self.type}'
